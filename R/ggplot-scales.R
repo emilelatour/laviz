16483340ed1a8c0a14423d989586eb23c0db22c0
Emile Latour <latour@ohsu.edu>
2025-06-11 16:57:28 -0700

    Examples for scale_color_collection

--- R/ggplot-scales.R
+++ R/ggplot-scales.R
@@ -6,6 +6,38 @@
 #' @param ... Other arguments passed to `scale_*_manual` or `scale_*_gradientn`
 #'
 #' @export
+#' @examples
+#' library(ggplot2)
+#' library(palmerpenguins)
+#'
+#' ggplot(data = penguins,
+#'        aes(x = flipper_length_mm,
+#'            y = body_mass_g)) +
+#'   geom_point(aes(color = species,
+#'                  shape = species),
+#'              size = 2) +
+#'   theme_minimal() +
+#'   laviz::scale_color_collection(collection = "personal",
+#'                                 palette = "sb_colorblind")
+#'
+#' ggplot(data = penguins,
+#'        aes(x = flipper_length_mm,
+#'            y = body_mass_g)) +
+#'   geom_point(aes(color = species,
+#'                  shape = species),
+#'              size = 2) +
+#'   theme_minimal() +
+#'   laviz::scale_color_collection(collection = "personal",
+#'                                 palette = "carter3a")
+#'
+#' ggplot(penguins,
+#'        aes(x = flipper_length_mm,
+#'            y = body_mass_g)) +
+#'   geom_point(aes(color = sex)) +
+#'   theme_minimal() +
+#'   laviz::scale_color_collection(collection = "personal",
+#'                                 palette = "binary") +
+#'   facet_wrap(~ species)
 scale_color_collection <- function(collection, palette, discrete = TRUE, ...) {
   pal <- get_palette(collection, palette, type = if (discrete) "discrete" else "continuous")
   if (discrete) {
@@ -17,6 +49,33 @@ scale_color_collection <- function(collection, palette, discrete = TRUE, ...) {
 
 #' @rdname scale_color_collection
 #' @export
+#' @examples
+#' library(ggplot2)
+#' library(palmerpenguins)
+#'
+#' ggplot(penguins,
+#'        aes(x = island,
+#'            fill = species)) +
+#'   geom_bar(alpha = 0.8) +
+#'   theme_minimal() +
+#'   facet_wrap(~species, ncol = 1) +
+#'   coord_flip() +
+#'   laviz::scale_fill_collection(collection = "sb",
+#'                                palette = "deep")
+#'
+#'
+#' ggplot(data = penguins,
+#'        aes(x = flipper_length_mm)) +
+#'   geom_histogram(aes(fill = species),
+#'                  alpha = 0.5,
+#'                  position = "identity",
+#'                  color = "white",
+#'                  binwidth = 2) +
+#'   theme_minimal() +
+#'   laviz::scale_fill_collection(collection = "personal",
+#'                                palette = "morosco3")
+#'
+
 scale_fill_collection <- function(collection, palette, discrete = TRUE, ...) {
   pal <- get_palette(collection, palette, type = if (discrete) "discrete" else "continuous")
   if (discrete) {
